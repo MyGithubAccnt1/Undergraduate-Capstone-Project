b493e367357502912f2ed86dcff9739f3c0b1adc
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-08-01 17:02:54 +0800

    Update preview.php

--- capstone/preview.php
+++ capstone/preview.php
@@ -52,53 +52,67 @@
 				
 				<div class="form-check form-switch d-flex flex-row justify-content-center mt-3">
 					<h5 class="text-center">COMMENTS "ON"</h5>
-				  	<input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+				  	<input class="form-check-input" type="checkbox" data-toggle="collapse" href="#collapseExample">
 				</div>
-				<hr class="w-75 mx-auto">
-				<div class="card p-3 mx-4">
-                    <div class="d-flex justify-content-between align-items-center">
-                      	<div class="d-flex flex-row align-items-center">
-                        	<span><small class="font-weight-bold text-primary">james_olesenn</small> <small class="font-weight-bold">Thanks</small></span>
-                      	</div>
-                      	<small>2 days ago</small>
-                    </div>
-                </div>
-                <div class="card p-3 mx-4 my-3">
-                    <div class="d-flex justify-content-between align-items-center">
-                      	<div class="d-flex flex-row align-items-center">
-                        	<span><small class="font-weight-bold text-primary">olan_sams</small> <small class="font-weight-bold">Loving your work and profile!</small></span>
-                      	</div>
-                      	<small>3 days ago</small>
-                    </div>
-                </div>
-                <div class="card p-3 mx-4 my-3">
-                    <div class="d-flex justify-content-between align-items-center">
-                      	<div class="d-flex flex-row align-items-center">
-                        	<span><small class="font-weight-bold text-primary">rashida_jones</small> <small class="font-weight-bold">Really cool Which filter are you using?</small></span>
-                      	</div>
-                      	<small>3 days ago</small>
-                    </div>
-                </div>
-                <div class="card p-3 mx-4 my-3">
-                    <div class="d-flex justify-content-between align-items-center">
-                      	<div class="d-flex flex-row align-items-center">
-                        	<span><small class="font-weight-bold text-primary">simona_rnasi</small> <small class="font-weight-bold">Hmm, This poster looks cool</small></span>
-                      	</div>
-                      	<small>3 days ago</small>
-                    </div>
-                </div>
+				<div class="row mt-2">
+	                            <div class="collapse" id="collapseExample">
+	                                <div class="stick-top bg-dark text-center text-white py-2">Comment Section</div>
+	                                <div class="border card-body" style="overflow-x:hidden; overflow-y:auto; height: 200px;">
+	                                    <div class="card p-3 mx-4">
+	                                        <div class="d-flex justify-content-between align-items-center">
+	                                            <div class="d-flex flex-row align-items-center">
+	                                                <span><small class="font-weight-bold text-primary">james_olesenn</small> <small class="font-weight-bold">Thanks</small></span>
+	                                            </div>
+	                                            <small>2 days ago</small>
+	                                        </div>
+	                                    </div>
+	                                    <div class="card p-3 mx-4 my-3">
+	                                        <div class="d-flex justify-content-between align-items-center">
+	                                            <div class="d-flex flex-row align-items-center">
+	                                                <span><small class="font-weight-bold text-primary">olan_sams</small> <small class="font-weight-bold">Loving your work and profile!</small></span>
+	                                            </div>
+	                                            <small>3 days ago</small>
+	                                        </div>
+	                                    </div>
+	                                    <div class="card p-3 mx-4 my-3">
+	                                        <div class="d-flex justify-content-between align-items-center">
+	                                            <div class="d-flex flex-row align-items-center">
+	                                                <span><small class="font-weight-bold text-primary">rashida_jones</small> <small class="font-weight-bold">Really cool Which filter are you using?</small></span>
+	                                            </div>
+	                                            <small>3 days ago</small>
+	                                        </div>
+	                                    </div>
+	                                    <div class="card p-3 mx-4 my-3">
+	                                        <div class="d-flex justify-content-between align-items-center">
+	                                            <div class="d-flex flex-row align-items-center">
+	                                                <span><small class="font-weight-bold text-primary">simona_rnasi</small> <small class="font-weight-bold">Hmm, This poster looks cool</small></span>
+	                                            </div>
+	                                            <small>3 days ago</small>
+	                                        </div>
+	                                    </div>
+	                                </div>
+	                                <div class="stick-bot">
+	                                    <div class="comment-area">
+	                                        <textarea class="form-control rounded-0" placeholder="Type your message here." rows="1"></textarea>
+	                                    </div>
+	                                    <div class="d-flex justify-content-center mt-3">
+	                                        <button class="btn btn-primary rounded-pill btn-md w-75">Send</button>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                        </div>
 				<div class="rating"> 
-                  	<input type="radio" name="rating" value="5" id="5"><label for="5">☆</label>
-                  	<input type="radio" name="rating" value="4" id="4"><label for="4">☆</label> 
-                  	<input type="radio" name="rating" value="3" id="3"><label for="3">☆</label>
-                  	<input type="radio" name="rating" value="2" id="2"><label for="2">☆</label>
-                  	<input type="radio" name="rating" value="1" id="1"><label for="1">☆</label>
-                  	<h5 style="margin-left: 30px; margin-top: 5px;">Rate: </h5>
-              	</div>
-              	<h5 style="margin-left: 30px; margin-top: 5px;">Add Comment </h5>
+		                  	<input type="radio" name="rating" value="5" id="5"><label for="5">☆</label>
+		                  	<input type="radio" name="rating" value="4" id="4"><label for="4">☆</label> 
+		                  	<input type="radio" name="rating" value="3" id="3"><label for="3">☆</label>
+		                  	<input type="radio" name="rating" value="2" id="2"><label for="2">☆</label>
+		                  	<input type="radio" name="rating" value="1" id="1"><label for="1">☆</label>
+		                  	<h5 style="margin-left: 30px; margin-top: 5px;">Rate: </h5>
+		              	</div>
+		              	<h5 style="margin-left: 30px; margin-top: 5px;">Add Comment </h5>
 				<div class="comment-area mx-4 my-3">
-                  	<textarea class="form-control" placeholder="What's on your mind?" rows="4"></textarea>
-              	</div>
+		                  	<textarea class="form-control" placeholder="What's on your mind?" rows="4"></textarea>
+		              	</div>
 				<div class="d-flex justify-content-center mb-3 ">
 					<button class="btn btn-success rounded-pill btn-md">Send ></button>
 				</div>
