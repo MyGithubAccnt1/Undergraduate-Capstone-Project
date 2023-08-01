f41a364e091edf339cbe0ddc0bf05ab0b78714d9
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-08-01 15:40:30 +0800

    Create customize.php

---
+++ capstone/customize.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	  	<meta charset="utf-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  	<title>Saint Benedict Medallion</title>
+	  	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	  	<?php include('include.php') ?>
+		<style>
+      
+		</style>
+	</head>
+	<body class="font-monospace bg-light">
+		<?php include('header.php') ?>
+		<main class="container-fluid my-4 my-width">
+		  <div class="row">
+        <div class="col-3">
+          
+        </div>
+      </div>
+		</main>
+		<?php include('footer.php') ?>
+		<script type="text/javascript">
+			var navigation = document.querySelector("header");
+			window.onload = navigation.classList.toggle('bg-dark');
+		</script>
+		<script>
+		    var toggleClick = document.querySelector(".box,.icon");
+		    var navigation = document.querySelector("header");
+		    var removeClick = document.querySelector(".close");
+		    toggleClick.addEventListener('click', ()=>{
+		        navigation.classList.toggle('active-nav');
+		    })
+		    removeClick.addEventListener('click', ()=>{
+		        navigation.classList.remove('active-nav');
+		    })
+		</script>
+  <body>
+<Html>
