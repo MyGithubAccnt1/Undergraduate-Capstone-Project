4c3b47146043090eb1ec801b90097701bbd3964c
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-08-01 19:28:52 +0800

    Update sample.php

--- capstone/sample.php
+++ capstone/sample.php
@@ -7,52 +7,65 @@
 	  	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 	  	<?php include('include.php') ?>
 		<style>
-		  	.bg{
-		  		position: absolute;
-		  		z-index: -2;
-		  		top: 0;
-		  		left: 0;
-		  		height: 100vh;
-		  		width: 100%;
-		  		background-image: url('images/bg.gif');
-		  		background-size: cover;
-		  		background-repeat: no-repeat;
-		  		filter: brightness(30%);
-		  	}
+			.product-card {
+			  	margin: 15px 0;
+			}
+			.product-button {
+				width: 50%;
+			}
 		</style>
 	</head>
 	<body class="font-monospace bg-light">
 		<?php include('header.php') ?>
-		<main class="container-fluid">
-			<?php 
-			include("connect.php");
-			$sql = "SELECT * FROM product";
-			$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
-	
-			$category_items = array();
-			while($row =mysqli_fetch_assoc($result)){
-				$category_items['id'] = $row['id'];
-				$category_items['category_id'] = $row['category_id'];
-				$category_items['price'] = $row['price'];
-				$category_items['title'] = $row['title'];
-				$category_items['thumbnail'] = $row['thumbnail'];
-				$category_items['link'] = $row['link'];
-				$category_items['sizes'] = $row['sizes'];
-			}
-			echo json_encode($category_items);
-			?>
-			<p><?php echo $category_items; ?></p>
+		<main class="container-fluid my-4 my-width p-0">
+			<div class="container">
+				<div class="row">
+				    <div class="col-sm-12 col-md-12 col-lg-4">
+					    <div class="card px-2">
+					        <h2 class="text-center">Filter by</h2>
+					        <h4 class="text-center">
+					        	<hr>Price<hr>
+					        </h4>
+					        <div class="card-body">
+						        <form id="price-range-form">
+						            <label for="min-price" class="form-label">Min price: </label>
+						            <span id="min-price-txt">₱0</span>
+						            <input type="range" class="form-range" min="0" max="999" id="min-price" step="1" value="0">
+						            <label for="max-price" class="form-label">Max price: </label>
+						            <span id="max-price-txt">₱1000</span>
+						            <input type="range" class="form-range" min="1" max="1000" id="max-price" step="1" value="1000">
+						        </form>
+					        </div>
+					    </div>
+				    </div>
+				    <div class="col-sm-12 col-md-12 col-lg-8">
+					    <div class="card">
+					        <div class="card-body">
+					        	<div class="d-flex justify-content-center">
+					        		<a href="customize.php">
+					        			<button class="btn btn-outline-success rounded-0 btn-sm">CUSTOMIZE</button>
+					        		</a>
+					        	</div>
+					          	<div class="carousel slide p-0" data-ride="carousel" data-interval="0">  
+					          		<!-- Wrapper for carousel items -->
+					          		<div class="carousel-inner" style="width: 100%">
+					          			<div class="item carousel-item active" style="overflow-x: auto;">
+					          				<div class="d-flex flex-direction-row gap-4" id="display-items-div" style="height: auto;">
+					          					
+					          				</div>
+					          			</div>
+					          		</div>
+					          	</div>
+					        </div>
+					    </div>
+				    </div>
+				</div>
+			</div>
 		</main>
 		<?php include('footer.php') ?>
-		<script>
-			var navbar = document.querySelector('header')
-			window.onscroll = function() {
-			  if (window.pageYOffset > 0) {
-			    navbar.classList.add('bg-dark')
-			  } else {
-			    navbar.classList.remove('bg-dark')
-			  }
-			}
+		<script type="text/javascript">
+			var navigation = document.querySelector("header");
+			window.onload = navigation.classList.toggle('bg-dark');
 		</script>
 		<script>
 		    var toggleClick = document.querySelector(".box,.icon");
@@ -66,39 +79,67 @@
 		    })
 		</script>
 		<script>
-			$(document).ready(function(){
-				$(".wish-icon i").click(function(){
-					$(this).toggleClass("fa-heart fa-heart-o");
-				});
-			});	
-		</script>
-		<script type="text/javascript" src="products.js"></script>
-		<script>
+			let category_items = '';
+			<?php
+			include("connect.php");
+			$sql = "SELECT * FROM product";
+			$result = mysqli_query($conn, $sql);
+			$row = mysqli_fetch_assoc($result);
+			?>
+			var id = <?php $row['id'] ?>;
+			category_items.forEach(function(item) {
+				item.id = id;
+			});
+			var category_id = <?php $row['category_id'] ?>;
+			category_items.forEach(function(item) {
+				item.category_id = category_id;
+			});
+			var price = <?php $row['price'] ?>;
+			category_items.forEach(function(item) {
+				item.price = price;
+			});
+			var title = <?php $row['title'] ?>;
+			category_items.forEach(function(item) {
+				item.title = title;
+			});
+			var thumbnail = <?php $row['thumbnail'] ?>;
+			category_items.forEach(function(item) {
+				item.thumbnail = thumbnail;
+			});
+			var link = <?php $row['link'] ?>;
+			category_items.forEach(function(item) {
+				item.link = link;
+			});
+			var sizes = <?php $row['sizes'] ?>;
+			category_items.forEach(function(item) {
+				item.sizes = sizes;
+			});
+			
+			let min_price = 0;
+			let max_price = 1000;
+
 			$(document).ready(function () {
 			  showAllItems(); //Display all items with no filter applied
 			});
+
+			$("#min-price").on("change mousemove", function () {
+			  min_price = parseInt($("#min-price").val());
+			  $("#min-price-txt").text("₱" + min_price);
+			  showItemsFiltered();
+			});
+
+			$("#max-price").on("change mousemove", function () {
+			  max_price = parseInt($("#max-price").val());
+			  $("#max-price-txt").text("₱" + max_price);
+			  showItemsFiltered();
+			});
+
 			function showAllItems() {
-				//Default grid to show all items on page load in
-				$("#one_sm_slideshow").empty();
-				for (let i = 0; i < category_items.length; i++) {
-				    let item_content =
-				    '<div class="col-sm-6 p-0" data-available-sizes="' + 
-				    category_items[i]["sizes"] + 
-				    '"><div class="thumb-wrapper border border-dark" style="width: 200px;"><span class="wish-icon"><i class="fa fa-heart-o"></i></span><div class="img-box"><img src="' + 
-				    category_items[i]["thumbnail"] +
-				    '" class="img-fluid" alt="Missing Image"></div><div class="thumb-content"><h4>' +
-				    category_items[i]["title"] +
-				    '</h4><p class="item-price"><b>₱' +
-				    category_items[i]["price"] +
-				    '</b></p><a href="' +
-				    category_items[i]["link"] +
-				    '" class="btn btn-primary">View</a></div></div></div>';
-				    $("#one_sm_slideshow").append(item_content);
-				}
-				$("#one_md_slideshow").empty();
-				for (let i = 0; i < category_items.length; i++) {
-				    let item_content =
-				    '<div class="container p-0 m-0" data-available-sizes="' + 
+			  //Default grid to show all items on page load in
+			  $("#display-items-div").empty();
+			  for (let i = 0; i < category_items.length; i++) {
+			    let item_content =
+			      '<div class="container p-0 m-0" data-available-sizes="' + 
 				    category_items[i]["sizes"] + 
 				    '"><div class="thumb-wrapper border border-dark m-0" style="width: 200px;"><span class="wish-icon"><i class="fa fa-heart-o"></i></span><div class="img-box"><img src="' + 
 				    category_items[i]["thumbnail"] +
@@ -108,40 +149,39 @@
 				    category_items[i]["price"] +
 				    '</b></p><a href="' +
 				    category_items[i]["link"] +
-				    '" class="btn btn-primary">View</a></div></div></div>';
-				    $("#one_md_slideshow").append(item_content);
+				    '"><button class="rounded-0 btn btn-outline-success btn-sm">View</button></a></div></div></div>';
+			    $("#display-items-div").append(item_content);
+			  }
+			}
+
+			function showItemsFiltered() {
+				let counter = 0;
+			  	//Default grid to show all items on page load in
+			  	$("#display-items-div").empty();
+			  	for (let i = 0; i < category_items.length; i++) {
+			    //Go through the items but only show items that have size from show_sizes_array
+				    if ( category_items[i]["price"] <= max_price && category_items[i]["price"] >= min_price ) {
+				      	let item_content =
+				        '<div class="container p-0 m-0" data-available-sizes="' + 
+				        category_items[i]["sizes"] + 
+				        '"><div class="thumb-wrapper border border-dark m-0" style="width: 200px;"><span class="wish-icon"><i class="fa fa-heart-o"></i></span><div class="img-box"><img src="' + 
+				        category_items[i]["thumbnail"] +
+				        '" class="img-fluid" alt="Missing Image"></div><div class="thumb-content"><h4>' +
+				        category_items[i]["title"] +
+				        '</h4><p class="item-price"><b>₱' +
+				        category_items[i]["price"] +
+				        '</b></p><a href="' +
+				        category_items[i]["link"] +
+				        '"><button class="rounded-0 btn btn-outline-success btn-sm">View</button></a></div></div></div>';
+				        $("#display-items-div").append(item_content); //Display in grid
+				    } else {
+				    	counter = counter + 1;
+				    }
 				}
-				$("#two_sm_slideshow").empty();
-				for (let i = 0; i < category_items.length; i++) {
+				if ( counter == 11 ) {
 				    let item_content =
-				    '<div class="col-sm-6 p-0" data-available-sizes="' + 
-				    category_items[i]["sizes"] + 
-				    '"><div class="thumb-wrapper border border-dark" style="width: 200px;"><span class="wish-icon"><i class="fa fa-heart-o"></i></span><div class="img-box"><img src="' + 
-				    category_items[i]["thumbnail"] +
-				    '" class="img-fluid" alt="Missing Image"></div><div class="thumb-content"><h4>' +
-				    category_items[i]["title"] +
-				    '</h4><p class="item-price"><b>₱' +
-				    category_items[i]["price"] +
-				    '</b></p><a href="' +
-				    category_items[i]["link"] +
-				    '" class="btn btn-primary">View</a></div></div></div>';
-				    $("#two_sm_slideshow").append(item_content);
-				}
-				$("#two_md_slideshow").empty();
-				for (let i = 0; i < category_items.length; i++) {
-				    let item_content =
-				    '<div class="container p-0 m-0" data-available-sizes="' + 
-				    category_items[i]["sizes"] + 
-				    '"><div class="thumb-wrapper border border-dark m-0" style="width: 200px;"><span class="wish-icon"><i class="fa fa-heart-o"></i></span><div class="img-box"><img src="' + 
-				    category_items[i]["thumbnail"] +
-				    '" class="img-fluid" alt="Missing Image"></div><div class="thumb-content"><h4>' +
-				    category_items[i]["title"] +
-				    '</h4><p class="item-price"><b>₱' +
-				    category_items[i]["price"] +
-				    '</b></p><a href="' +
-				    category_items[i]["link"] +
-				    '" class="btn btn-primary">View</a></div></div></div>';
-				    $("#two_md_slideshow").append(item_content);
+				    '<div class="col-sm-12 col-md-6 col-lg-6 text-center product-card mx-auto"><b>No Item Found.</b></div>';
+				    $("#display-items-div").append(item_content); //Display in grid
 				}
 			}
 		</script>
