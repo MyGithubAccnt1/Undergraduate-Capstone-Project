a8d563d509a6d35659790531dcf2979ded22a815
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-08-02 23:06:53 +0800

    Update get_comments.php

--- capstone/get_comments.php
+++ capstone/get_comments.php
@@ -2,27 +2,18 @@
 session_start();
 include("connect.php");
 $title = $_SESSION['title'];
-
+$pdo = new PDO('mysql:host=localhost;dbname=sbm;charset=utf8', 'username', 'password');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // Fetch comments from the database
 $sql = "SELECT * FROM comments WHERE title = '$title' ORDER BY id DESC";
-$result = mysqli_query($conn, $sql);
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Checking if the query was successful
-if (mysqli_num_rows($result) == 0) {
-    // No rows were returned, so the result is empty
-   echo '<div class="comment card p-3 mx-4">';
-    echo '<div class="d-flex justify-content-between align-items-center">';
-    echo '<div class="d-flex flex-row align-items-center">';
-    echo '<span><small class="font-weight-bold text-warning">[';
-    echo 'Administrator]</small> ';
-    echo '<small class="font-weight-bold">says: ';
-    echo 'Be the first one to leave your comment.</small></span>';
-    echo '</div>';
-    echo '<small class="text-danger">Verified</small>';
-    echo '</div>';
-    echo '</div>';
-} else {
-    // Rows were returned, so you can display the data as usual
+if ($stmt->rowCount() > 0) {
+    // Database has data, so proceed with displaying the comments
     while ($row = mysqli_fetch_assoc($result)) {
         echo '<div class="comment card p-3 mx-4">';
         echo '<div class="d-flex justify-content-between align-items-center">';
@@ -36,7 +27,21 @@ if (mysqli_num_rows($result) == 0) {
         echo '</div>';
         echo '</div>';
     }
+    // Don't forget to close the database connection when you're done with it
     mysqli_free_result($result);
+} else {
+    // Database is empty, so display a message or perform other actions
+    echo '<div class="comment card p-3 mx-4">';
+    echo '<div class="d-flex justify-content-between align-items-center">';
+    echo '<div class="d-flex flex-row align-items-center">';
+    echo '<span><small class="font-weight-bold text-warning">[';
+    echo 'Administrator]</small> ';
+    echo '<small class="font-weight-bold">says: ';
+    echo 'Be the first one to leave your comment.</small></span>';
+    echo '</div>';
+    echo '<small class="text-danger">Verified</small>';
+    echo '</div>';
+    echo '</div>';
 }
 // Don't forget to close the database connection when you're done with it
 mysqli_close($conn);
