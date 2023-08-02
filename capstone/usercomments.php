9a2ef0bf1b70159c1ee47e70a1195bc25d938076
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-08-02 18:21:34 +0800

    Update usercomments.php

--- capstone/usercomments.php
+++ capstone/usercomments.php
@@ -4,26 +4,15 @@ include("connect.php");
 $name_entered= $_POST['name'];
 $comment_entered= $_POST['comment'];
 $table= $_POST['webpage'];
-
-$date= date("m-d-Y");
-
-$val = mysql_query("select 1 from $table");
-
-if($val !== FALSE)
-{
-  if ((!empty($name_entered)) && (!empty($comment_entered)))
-  {
-    mysql_query("INSERT INTO $table (name, date, comments)
-    VALUES ('$name_entered', '$date', '$comment_entered')");
-  }
-  $result= mysql_query( "SELECT * FROM $table ORDER BY ID DESC" ) 
-  or die("SELECT Error: ".mysql_error()); 
-
+$date = date('Y-m-d H:i');
+$sql = ("INSERT INTO $table (name, date, comments) VALUES ('$name_entered', '$date', '$comment_entered')");
+if ($conn->query($sql) === TRUE) {
+  $sql = ( "SELECT * FROM $table ORDER BY ID DESC" ) or die("SELECT Error: ".mysql_error()); 
+  $result = mysqli_query($conn, $sql);
   while ($row = mysql_fetch_array($result)){ 
     $name_field= $row['name'];
     $date_field= $row['date'];
     $comment_field= $row['comments'];
-  
     echo '<div class="card p-3 mx-4">';
     echo '<div class="d-flex justify-content-between align-items-center">';
     echo '<div class="d-flex flex-row align-items-center">';
