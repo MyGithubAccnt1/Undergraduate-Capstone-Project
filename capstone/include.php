fe0aa3b5f9f915c2a43c0cb41d6ee8e82af9700a
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-08-02 22:31:32 +0800

    Update include.php

--- capstone/include.php
+++ capstone/include.php
@@ -13,6 +13,7 @@
 
 <!-- jquery 3.5.1 js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <!-- popper 1.16.0 js -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
