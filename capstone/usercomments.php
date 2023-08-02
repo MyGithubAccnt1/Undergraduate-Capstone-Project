de08052822d93732f71ce6879da10f99d5daf814
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-08-02 11:56:32 +0800

    Create usercomments.php

---
+++ capstone/usercomments.php
@@ -0,0 +1,70 @@
+<?php
+session_start(); 
+include("connect.php");
+
+$name_entered= $_POST['name'];
+$comment_entered= $_POST['comment'];
+$table= $_POST['webpage'];
+
+$date= date("m-d-Y");
+
+mysql_select_db($dbase, $connection);
+
+$val = mysql_query("select 1 from $table");
+
+if($val !== FALSE)
+{
+  if ((!empty($name_entered)) && (!empty($comment_entered)))
+  {
+    mysql_query("INSERT INTO $table (name, date, comments)
+    VALUES ('$name_entered', '$date', '$comment_entered')");
+  }
+  $result= mysql_query( "SELECT * FROM $table ORDER BY ID DESC" ) 
+  or die("SELECT Error: ".mysql_error()); 
+
+  while ($row = mysql_fetch_array($result)){ 
+    $name_field= $row['name'];
+    $date_field= $row['date'];
+    $comment_field= $row['comments'];
+  
+    echo "$name_field wrote: ($date_field) <br>";
+    echo "$comment_field";
+    echo "<br><hr><br>";
+  }
+}
+else
+{
+$createtable= "CREATE TABLE $table
+( ".
+"ID INT NOT NULL AUTO_INCREMENT, ".
+"name VARCHAR(50) NOT NULL, ".
+"date VARCHAR(50) NOT NULL, ".
+"comments VARCHAR(60000) NOT NULL, ".
+"PRIMARY KEY (ID)
+);
+";
+
+$create= mysql_query($createtable, $connection);
+
+if ($create)
+{
+  if ((!empty($name_entered)) && (!empty($comment_entered)))
+  {
+    mysql_query("INSERT INTO $table (name, date, comments)
+    VALUES ('$name_entered', '$date', '$comment_entered')");
+  }
+  $result= mysql_query( "SELECT * FROM $table ORDER BY ID DESC" ) 
+  or die("SELECT Error: ".mysql_error()); 
+
+  while ($row = mysql_fetch_array($result)){ 
+    $name_field= $row['name'];
+    $date_field= $row['date'];
+    $comment_field= $row['comments'];
+  
+    echo "$name_field wrote: ($date_field) <br>";
+    echo "$comment_field";
+    echo "<br><hr><br>";
+  }
+}//if createtable
+}//else
+?> 
