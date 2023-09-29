ed47e7518b88ffa4d57fbdbcea53c48c0fad4b81
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-09-29 13:12:10 +0800

    Add files via upload

---
+++ capstone/customize.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	  	<meta charset="utf-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  	<title>Saint Benedict Medallion</title>
+	  	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	  	<?php include('./include/style.php') ?>
+		<style>
+			.btn-customize {
+			    background-color: #794B29;
+			    color: white;
+			}
+			.my-button:hover, .active {
+				background-color: rgb(0, 0, 0, 0.1);
+			}
+			.material-button {
+				border: none;
+				background-color: inherit;
+			}
+		</style>
+	</head>
+	<body class="font-monospace">
+		<?php include('./include/header.php') ?>
+		<main class="container-fluid m-0 p-0">
+			<section class="text-center my-5 py-5">
+				[This page is under maintenance]
+			</section>
+		</main>
+		<?php include('./include/footer.php') ?>
+	</body>
+</html>
