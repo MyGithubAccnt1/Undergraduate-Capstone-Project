84a9106bee5191d4fd3214a316a8a4c32eb294ee
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-09-29 11:59:01 +0800

    Add files via upload

--- capstone/php/get_total_estimate_percent.php
+++ capstone/php/get_total_estimate_percent.php
@@ -1,7 +1,11 @@
 <?php
 include("connect.php");
 
-$sql = "SELECT COUNT(*) AS total_rows FROM history WHERE status = 'Pending' OR status = 'On-The-Way'";
+$totalOrder = 0;
+$totalEstimate = 0;
+$estimatePercent = 0;
+
+$sql = "SELECT COUNT(*) AS total_rows FROM history";
 $result = $conn->query($sql);
 
 if ($result === false) {
@@ -9,12 +13,32 @@ if ($result === false) {
 } else {
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        $totalRows = $row["total_rows"];
-        echo $totalRows;
+        $totalOrder = $row["total_rows"];
     } else {
-        echo $totalRows;
+
     }
 }
+
+$newsql = "SELECT COUNT(*) AS total_rows FROM history WHERE status = 'Pending' OR status = 'On-The-Way'";
+$newresult = $conn->query($newsql);
+
+if ($newresult === false) {
+    echo "Query error: " . $conn->error;
+} else {
+    if ($newresult->num_rows > 0) {
+        $row = $newresult->fetch_assoc();
+        $totalEstimate = $row["total_rows"];
+    } else {
+
+    }
+}
+
+if ($totalOrder > 0) {
+    $estimatePercent = ($totalEstimate / $totalOrder) * 100;
+    $estimatePercent = number_format($estimatePercent, 2);
+}
+
+echo $estimatePercent;
 
 $conn->close();
 ?>
