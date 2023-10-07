365b9b135cb8258154028d922f4a7520e5321ec5
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-10-07 12:34:57 +0800

    Add files via upload

--- capstone/php/delete_template.php
+++ capstone/php/delete_template.php
@@ -5,29 +5,34 @@ $deyt = $_POST["deyt"];
 $email = mysqli_real_escape_string($conn, $email);
 $deyt = mysqli_real_escape_string($conn, $deyt);
 
-$objectsql = "DELETE FROM object WHERE email = '$email' and deyt = '$deyt'";
-if (mysqli_query($conn, $objectsql)) {
+$checksql = "SELECT * FROM history WHERE email = '$email' and deyt = '$deyt'";
+$checkresult = mysqli_query($conn, $checksql);
+if (mysqli_num_rows($checkresult) > 0) {
+    exit;
+} else {
+    $objectsql = "DELETE FROM object WHERE email = '$email' and deyt = '$deyt'";
+    if (mysqli_query($conn, $objectsql)) {
 
-    $gettemplatesql = "SELECT thumbnail FROM template WHERE email = '$email' and deyt = '$deyt'";
-    $result = mysqli_query($conn, $gettemplatesql);
-    if (mysqli_num_rows($result) > 0) {
-         $row = $result->fetch_assoc();
-         $imageFile = $row['thumbnail'];
-         if (file_exists("../" . $imageFile)) {
-            unlink("../" . $imageFile);
-         }
-    }
+        $gettemplatesql = "SELECT thumbnail FROM template WHERE email = '$email' and deyt = '$deyt'";
+        $result = mysqli_query($conn, $gettemplatesql);
+        if (mysqli_num_rows($result) > 0) {
+             $row = $result->fetch_assoc();
+             $imageFile = $row['thumbnail'];
+             if (file_exists("../" . $imageFile)) {
+                unlink("../" . $imageFile);
+             }
+        }
 
-    $templatesql = "DELETE FROM template WHERE email = '$email' and deyt = '$deyt'";
-    if (mysqli_query($conn, $templatesql)) {
+        $templatesql = "DELETE FROM template WHERE email = '$email' and deyt = '$deyt'";
+        if (mysqli_query($conn, $templatesql)) {
+
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
 
     } else {
         echo "Error: " . mysqli_error($conn);
     }
-
-} else {
-    echo "Error: " . mysqli_error($conn);
 }
-
 mysqli_close($conn);
 ?>
