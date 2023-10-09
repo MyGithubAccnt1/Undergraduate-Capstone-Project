d740a425ad4b535fb09459efdae748fdaf9b21d9
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-10-10 03:48:02 +0800

    Add files via upload

--- capstone/php/order_template.php
+++ capstone/php/order_template.php
@@ -6,6 +6,11 @@ $date = $_SESSION['date'];
 $title = "Customize Item";
 $total = 0;
 $status = "For Review";
+$input_fname = $_POST['fname'];
+$input_lname = $_POST['lname'];
+$input_mnumber = $_POST['mnumber'];
+$input_email = $_POST['email'];
+$input_caddress = $_POST['caddress'];
 
 $checksql = "SELECT * FROM history WHERE email = ? and deyt = ?";
 $checkstmt = $conn->prepare($checksql);
@@ -15,13 +20,17 @@ $checkresult = $checkstmt->get_result();
 
 if ($checkresult->num_rows > 0) {
 
+    echo "1";
+
 } else {
 
-    $insertSql = "INSERT INTO history (email, title, total, deyt, status) VALUES (?, ?, ?, ?, ?)";
+    $insertSql = "INSERT INTO history (email, title, total, deyt, status, input_fname, input_lname, input_mnumber, input_email, input_caddress) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $insertStmt = $conn->prepare($insertSql);
-    $insertStmt->bind_param("ssdss", $email, $title, $total, $date, $status);
+    $insertStmt->bind_param("ssdsssssss", $email, $title, $total, $date, $status, $input_fname, $input_lname, $input_mnumber, $input_email, $input_caddress);
 
     if ($insertStmt->execute()) {
+
+        echo "2";
 
         $notifmessage = "[". $email ."] successfully completed an order of [". $title ."].";
         $notifcategory = "order";
@@ -29,9 +38,9 @@ if ($checkresult->num_rows > 0) {
         $notifresult = mysqli_query($conn, $notifsql);
 
     } else {
-        echo "Error making order: " . $insertStmt->error;
-        sleep(2);
-        header("Location: ../make-customize.php");
+
+        echo "3";
+
     }
 
 }
