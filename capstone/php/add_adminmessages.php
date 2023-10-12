946cc8c575ff1664cf7c69a6b0479da6571cf5fc
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-10-12 20:56:15 +0800

    Add files via upload

--- capstone/php/add_adminmessages.php
+++ capstone/php/add_adminmessages.php
@@ -7,28 +7,21 @@ $id = $_POST["id"];
 $email = $_POST["email"];
 $role = "Admin";
 $date = $_POST["date"];
-$comment = $_POST["comment"];
+$comment = mysqli_real_escape_string($conn, $_POST["comment"]);
 
-// Escape user input to prevent SQL injection (not secure, use prepared statements in production)
-$email = mysqli_real_escape_string($conn, $email);
-$comment = mysqli_real_escape_string($conn, $comment);
-
-// Insert the comment into the database
 if ($comment === "" || $date === "") {
-
+    echo "1";
 } else {
 
-    $sql = "INSERT INTO message (sender, email, message, deyt, role) VALUES ('$id', '$email', '$comment', '$date', '$role')";
-    // Execute the SQL statement here (e.g., using a database connection)
-    if (mysqli_query($conn, $sql)) {
-        // Comment added successfully
+    $sql = "INSERT INTO message (sender, email, message, deyt, role) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssss", $id, $email, $comment, $date, $role);
+    if ($stmt->execute()) {
+        echo "2";
     } else {
-        // Error inserting comment
-        echo "Error: " . mysqli_error($conn);
+        echo "3";
     }
-
+    $stmt->close();
 }
-
-// Close the database connection
 mysqli_close($conn);
 ?>
