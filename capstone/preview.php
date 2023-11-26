eed299904bc75a7f84fd7f674aa290892aa77ac8
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-11-26 17:43:45 +0800

    Add files via upload

--- capstone/preview.php
+++ capstone/preview.php
@@ -63,14 +63,14 @@ if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
 						</div>
 					</div>
 				</div>
-				<div class="rating container-fluid my-width m-0 p-0 mx-auto"> 
+				<!-- <div class="rating container-fluid my-width m-0 p-0 mx-auto"> 
                   	<input type="radio" name="rating" value="5" id="5"><label for="5">☆</label>
                   	<input type="radio" name="rating" value="4" id="4"><label for="4">☆</label> 
                   	<input type="radio" name="rating" value="3" id="3"><label for="3">☆</label>
                   	<input type="radio" name="rating" value="2" id="2"><label for="2">☆</label>
                   	<input type="radio" name="rating" value="1" id="1"><label for="1">☆</label>
                   	<h5 style="margin-left: 30px; margin-top: 5px;">Rate: </h5>
-          		</div>
+          		</div> -->
 				<div class="container-fluid my-width m-0 p-0 mx-auto border border-dark">
 					<div class="form-switch p-0 d-flex justify-content-center">
 						<h5 class="my-auto">COMMENT</h5>
