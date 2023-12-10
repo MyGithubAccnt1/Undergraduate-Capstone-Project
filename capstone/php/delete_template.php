840ba111a0d00b8311cc395fa9a03d409a0aea0e
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2023-12-11 00:48:10 +0800

    Add files via upload

--- capstone/php/delete_template.php
+++ capstone/php/delete_template.php
@@ -9,29 +9,54 @@ if (mysqli_num_rows($checkresult) > 0) {
     echo "1";
     exit;
 } else {
-    $objectsql = "DELETE FROM object WHERE email = '$email' and deyt = '$deyt'";
-    if (mysqli_query($conn, $objectsql)) {
+    $front = "";
+    $back = "";
+    $gettemplatesql = "SELECT front, back FROM template WHERE email = '$email' and deyt = '$deyt'";
+    $result = mysqli_query($conn, $gettemplatesql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = $result->fetch_assoc();
+        $front = $row['front'];
+        $back = $row['back'];
 
-        $gettemplatesql = "SELECT thumbnail FROM template WHERE email = '$email' and deyt = '$deyt'";
-        $result = mysqli_query($conn, $gettemplatesql);
-        if (mysqli_num_rows($result) > 0) {
-             $row = $result->fetch_assoc();
-             $imageFile = $row['thumbnail'];
-             if (file_exists("../" . $imageFile)) {
-                unlink("../" . $imageFile);
-             }
+        if (empty($front)) {
+
+        } else {
+             
+            $objectsql = "DELETE FROM object WHERE email = '$email' and deyt = '$front'";
+
         }
 
-        $templatesql = "DELETE FROM template WHERE email = '$email' and deyt = '$deyt'";
-        if (mysqli_query($conn, $templatesql)) {
+        if (empty($back)) {
+
+        } else {
+             
+            $objectsql = "DELETE FROM object WHERE email = '$email' and deyt = '$back'";
+             
+        }
+
+        if (mysqli_query($conn, $objectsql)) {
+
+            $gettemplatesql = "SELECT thumbnail FROM template WHERE email = '$email' and deyt = '$deyt'";
+            $result = mysqli_query($conn, $gettemplatesql);
+            if (mysqli_num_rows($result) > 0) {
+                $row = $result->fetch_assoc();
+                $imageFile = $row['thumbnail'];
+                if (file_exists("../" . $imageFile)) {
+                    unlink("../" . $imageFile);
+                }
+            }
+
+            $templatesql = "DELETE FROM template WHERE email = '$email' and deyt = '$deyt'";
+            if (mysqli_query($conn, $templatesql)) {
+
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
 
         } else {
             echo "Error: " . mysqli_error($conn);
         }
-
-    } else {
-        echo "Error: " . mysqli_error($conn);
-    }
+    }    
 }
 mysqli_close($conn);
 ?>
