7b6dceb9da7375812ed1682b281f667272d0f313
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-01-01 01:18:40 +0800

    Add files via upload

--- capstone/js/checkout_order_summary.js
+++ capstone/js/checkout_order_summary.js
@@ -1,33 +1,46 @@
 function alt_address() {
+    console.log('Function started');
 
-	if ($('#alt-address').val() === "") {
+    if ($('#alt-address').val() === "") {
+        console.log('Alternative address is empty');
 
-		if ("geolocation" in navigator) {
-		    navigator.geolocation.getCurrentPosition(function(position) {
-		        var latitude = position.coords.latitude;
-		        var longitude = position.coords.longitude;
-		        var currentOrigin = window.location.origin;
-		        var apiUrl = currentOrigin + ':3000/get-nominatim-data?format=json&lat=' + latitude + '&lon=' + longitude;
-		        $.ajax({
-	                url: apiUrl,
-	                type: 'GET',
-	                success: function(data) {
-	                    if (data && data.display_name) {
-	                        var formattedAddress = data.display_name;
-	                        $('#alt-address').val(formattedAddress);
-	                        $('#alt-address').focus();
-	                        $('#alternative_address').text('Alternative: ' + $('#alt-address').val());
-	                    }
-	                }
-	            });
-		    });
-		}
+        if ("geolocation" in navigator) {
+            console.log('Geolocation supported');
 
-	} else {
-		$('#alternative_address').text('Alternative: ' + $('#alt-address').val());
-	}
-	
+            navigator.geolocation.getCurrentPosition(function(position) {
+                console.log('Got geolocation data:', position);
+
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
+                var currentOrigin = window.location.origin;
+                var apiUrl = currentOrigin + ':3000/get-nominatim-data?format=json&lat=' + latitude + '&lon=' + longitude;
+                console.log('API URL:', apiUrl);
+
+                $.ajax({
+                    url: apiUrl,
+                    type: 'GET',
+                    success: function(data) {
+                        console.log('AJAX success:', data);
+
+                        if (data && data.display_name) {
+                            var formattedAddress = data.display_name;
+                            $('#alt-address').val(formattedAddress);
+                            $('#alt-address').focus();
+                            $('#alternative_address').text('Alternative: ' + $('#alt-address').val());
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('AJAX error:', status, error);
+                    }
+                });
+            });
+        }
+    } else {
+        console.log('Alternative address is not empty');
+        $('#alternative_address').text('Alternative: ' + $('#alt-address').val());
+    }
 }
+
 
 $(document).on('input change', '#alt-address', function() {
 	alt_address();
