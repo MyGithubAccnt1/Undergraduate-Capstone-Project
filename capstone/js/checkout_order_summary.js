eff21c16b6bc352ce4eeaf3ebb55a6feddba63a0
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-01-01 00:32:17 +0800

    Add files via upload

---
+++ capstone/js/checkout_order_summary.js
@@ -0,0 +1,56 @@
+function alt_address() {
+
+	if ($('#alt-address').val() === "") {
+
+		if ("geolocation" in navigator) {
+		    navigator.geolocation.getCurrentPosition(function(position) {
+		        var latitude = position.coords.latitude;
+		        var longitude = position.coords.longitude;
+
+		        var nominatimApiUrl = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + latitude + "&lon=" + longitude;
+
+	            $.ajax({
+	                url: nominatimApiUrl,
+	                type: 'GET',
+	                success: function(data) {
+	                    if (data && data.display_name) {
+	                        var formattedAddress = data.display_name;
+	                        $('#alt-address').val(formattedAddress);
+	                        $('#alt-address').focus();
+	                        $('#alternative_address').text('Alternative: ' + $('#alt-address').val());
+	                    }
+	                }
+	            });
+		    });
+		}
+
+	} else {
+		$('#alternative_address').text('Alternative: ' + $('#alt-address').val());
+	}
+	
+}
+
+$(document).on('input change', '#alt-address', function() {
+	alt_address();
+});
+
+$(document).ready(function() {
+	var currentDate = new Date();
+	var readyDate = currentDate.toISOString().slice(0,10);
+	$('#date').text('Date: ' + readyDate);
+	$('#full_name').text('Buyer: ' + $('#fname').val() + ' ' + $('#lname').val());
+	$('#mobile_number').text('Number: ' + $('#mnumber').val());
+	$('#complete_address').text('Address: ' + $('#caddress').val());
+});
+
+function ShowCart() {
+    $.ajax({
+        url: "./php/get_checkout_cart.php",
+        method: "GET",
+        success: function (data) {
+            data = data.trim();
+            $("#cart-container").html(data);
+        }
+    });
+}
+setInterval(ShowCart, 1000);
