18c5b2a93ffef7cbb82f0798f83a34bd2a593e94
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-01-02 01:49:24 +0800

    Add files via upload

--- capstone/php/get_you_may_like.php
+++ capstone/php/get_you_may_like.php
@@ -1,19 +1,17 @@
 <?php
 include("connect.php");
-$category = mysqli_real_escape_string($conn, $_GET['category']);
-
-$sql = "SELECT * FROM product WHERE category = '$category' ORDER BY `popularity` DESC LIMIT 3 OFFSET 0";
+$sql = "SELECT * FROM product ORDER BY popularity ASC LIMIT 4 OFFSET 0";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		echo '<div class="col-sm-12 col-md-3">';
-			echo '<div class="thumb-wrapper">';
+		echo '<div class="col-sm-12 col-md-4 col-lg-3 mb-3">';
+			echo '<div class="thumb-wrapper">';	
 				echo '<div class="img-box">';
 					echo '<img src="' . $row['thumbnail'] . '" class="img-responsive" alt="Missing Image">';
 					echo '<input type="hidden" name="image" value="' . $row['thumbnail'] . '">';
 				echo '</div>';
 				echo '<div class="thumb-content">';
-					echo '<form action="" id="viewProduct">';;
+					echo '<form action="" id="viewProduct">';
 						echo '<input type="hidden" name="title" value="' . $row['title'] . '">';
 						echo '<input type="hidden" name="thumbnail" value="' . $row['thumbnail'] . '">';
 						echo '<input type="hidden" name="price" value="' . $row['price'] . '">';
@@ -28,16 +26,9 @@ if ($result->num_rows > 0) {
 			echo '</div>';
 		echo '</div>';
 	}
-	echo '<div class="col-sm-12 col-md-3 view-all">';
-		echo '<div class="thumb-wrapper">';
-			echo '<div class="thumb-content">';
-				echo '<a href="logos.php" class="btn">View All</a>';
-			echo '</div>';
-		echo '</div>';
-	echo '</div>';
 } else {
 	echo '<p class="w-100 text-center text-dark">[There is no available product at the moment]</p>';
-	
 }
+
 mysqli_close($conn);
 ?>
