60e01c7a7c6796d1a9a3e403ca9822089b243c17
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-01-04 02:08:38 +0800

    Add files via upload

--- capstone/checkout_order_summary.php
+++ capstone/checkout_order_summary.php
@@ -10,7 +10,11 @@ if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
 	  	<meta charset="UTF-8">
 	  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  	<title>Saint Benedict Medallion</title>
-	  	<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+	  	<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+		<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+		<link rel="manifest" href="images/site.webmanifest">
 	  	<meta name="description" content="In partial fulfilment of the requirements for the degree of Bachelor of Science in Information Technology">
 	  	<meta name="keywords" content="capstone, project, thesis">
 	  	<meta name="author" content="Mhel Voi A. Bernabe">
