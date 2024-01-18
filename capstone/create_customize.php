54b4e26065536381da4e8d383755d1cfabc62db0
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-01-18 13:33:51 +0800

    Add files via upload

--- capstone/create_customize.php
+++ capstone/create_customize.php
@@ -200,38 +200,18 @@ session_start();
 								</div>
 							</div>
 
-							<div class="custom-collapse collapse py-5" id="necklace_text">
+							<div class="custom-collapse collapse py-5" id="necklace_engrave">
 							  	<div class="row gy-3">
 							  		<div class="col-6 d-flex justify-content-start">
-							  			<button type="button" class="btn btn-outline-light rounded-0 w-50 back_necklace_text">Back</button>
+							  			<button type="button" class="btn btn-outline-light rounded-0 w-50 back_necklace_engrave">Back</button>
 							  		</div>
-									<h1 class="mb-3">Insert Text</h1>
+									<h1 class="mb-3">Select Engrave</h1>
 									<hr>
-									<small>THIS TEXT WILL BE ENGRAVE TO YOUR NECKLACE</small>
 									<div class="col-12">
-										<form id="necklace_text_form">
-										    <div class="comment-area">
-										    	<div style="display: flex; flex-direction: row; gap: 10px; align-items: center;">
-		            								<span style="margin: 0 5px 0 7px;">Font Style:</span>
-		            								<select id="necklace_text_font">
-		                                    	        <option value="Arial">Arial</option>
-		                                    	        <option value="Courier" selected>Courier</option>
-		            									<option value="Helvetica">Helvetica</option>
-		            									<option value="Impact">Impact</option>
-		            									<option value="Segoe UI">Segoe UI</option>
-		            									<option value="Times New Roman">Times New Roman</option>
-		            									<option value="Verdana">Verdana</option>
-		                                    	    </select>
-										        </div>
-										        <div class="w-100 p-1" style="display: flex; flex-direction: row; gap: 10px; align-items: center;">
-										        	<span>Company:</span>
-										            <textarea class="form-control rounded-pill" rows="2" name="necklace_text" required></textarea>
-										        </div>
-										        <div class="w-75 mx-auto p-1">
-										        	<button type="submit" class="btn btn-sm btn-primary py-1 w-100 rounded-pill">Done</button>
-										        </div>
-										    </div>
-										</form>
+										<button type="button" id="necklace_engrave_text" class="btn btn-outline-light rounded-0 w-50">Text</button>
+									</div>
+									<div class="col-12">
+										<button type="button" id="necklace_engrave_image" class="btn btn-outline-light rounded-0 w-50">Image</button>
 									</div>
 								</div>
 							</div>
@@ -278,6 +258,42 @@ session_start();
 										        <div class="w-100 p-1" style="display: flex; flex-direction: row; gap: 10px; align-items: center;">
 										        	<span>Company:</span>
 										            <textarea class="form-control rounded-pill" rows="2" name="logo_seal_company" required></textarea>
+										        </div>
+										        <div class="w-75 mx-auto p-1">
+										        	<button type="submit" class="btn btn-sm btn-primary py-1 w-100 rounded-pill">Done</button>
+										        </div>
+										    </div>
+										</form>
+									</div>
+								</div>
+							</div>
+
+							<div class="custom-collapse collapse py-5" id="necklace_text">
+							  	<div class="row gy-3">
+							  		<div class="col-6 d-flex justify-content-start">
+							  			<button type="button" class="btn btn-outline-light rounded-0 w-50 back_necklace_text">Back</button>
+							  		</div>
+									<h1 class="mb-3">Insert Text</h1>
+									<hr>
+									<small>THIS TEXT WILL BE ENGRAVE TO YOUR NECKLACE</small>
+									<div class="col-12">
+										<form id="necklace_text_form">
+										    <div class="comment-area">
+										    	<div style="display: flex; flex-direction: row; gap: 10px; align-items: center;">
+		            								<span style="margin: 0 5px 0 7px;">Font Style:</span>
+		            								<select id="necklace_text_font">
+		                                    	        <option value="Arial">Arial</option>
+		                                    	        <option value="Courier" selected>Courier</option>
+		            									<option value="Helvetica">Helvetica</option>
+		            									<option value="Impact">Impact</option>
+		            									<option value="Segoe UI">Segoe UI</option>
+		            									<option value="Times New Roman">Times New Roman</option>
+		            									<option value="Verdana">Verdana</option>
+		                                    	    </select>
+										        </div>
+										        <div class="w-100 p-1" style="display: flex; flex-direction: row; gap: 10px; align-items: center;">
+										        	<span>Company:</span>
+										            <textarea class="form-control rounded-pill" rows="2" name="necklace_text" required></textarea>
 										        </div>
 										        <div class="w-75 mx-auto p-1">
 										        	<button type="submit" class="btn btn-sm btn-primary py-1 w-100 rounded-pill">Done</button>
