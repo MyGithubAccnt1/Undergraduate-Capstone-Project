ece2562fb04252c13f55600edaf4760212d213c7
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-01-26 18:42:18 +0800

    Update style.php

--- capstone/admin/include/style.php
+++ capstone/admin/include/style.php
@@ -6,8 +6,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <!-- popper 1.16.0 js -->
 <!-- <script type="text/javascript" src="../include/popper.min.js"></script> -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
-<!-- jquery 3.6.1 js
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<!-- jquery 3.6.1 js -->
 <!-- <script type="text/javascript" src="../include/jquery-3.6.1.min.js"></script> -->
 <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha384-Vvooey8iu18IIP2UsBgOl+DI0kQR66LQqPBthxB5y4YqaRgugWOu+vaqwY/AGNAb" crossorigin="anonymous"></script>
 <!-- fontawesome 5.15.4 css -->
