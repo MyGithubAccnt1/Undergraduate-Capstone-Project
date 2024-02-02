c237e2f1dd3a583cdd9813e610786192c32160a2
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-02-02 20:45:22 +0800

    Add files via upload

--- capstone/admin/php/get_messages.php
+++ capstone/admin/php/get_messages.php
@@ -3,6 +3,7 @@ session_start();
 include("connect.php");
 $data = $_GET['data'];
 $data = explode(' ', $data);
+$date = null;
 $sql = '';
 if ($data[1]) {
     $date = $data[1] . ' ' . $data[2];
@@ -47,19 +48,15 @@ if (mysqli_num_rows($result) > 0) {
         }
 
     }
-    mysqli_free_result($result);
-} else {
-    echo '<div class="card p-3" style="width: 95%; margin: 20px auto; transform: scaleY(-1);">';
-        echo '<div class="d-flex justify-content-between align-items-center">';
-            echo '<div class="d-flex flex-row align-items-center">';
-                echo '<span>';
-                    echo '<small class="font-weight-bold text-warning">[Administrator]</small>';
-                    echo '<small>: Do not hesitate to ask questions.</small>';
-                echo '</span>';
-            echo '</div>';
-            echo '<small class="text-warning">Verified</small>';
-        echo '</div>';
+    echo '<div class="text-center text-danger w-100" style="transform: scaleY(-1); position: absolute; top: 10px;">';
+        if ($date) {
+            echo '<small>ORDER DATE: '. $date .'</small>';
+        } else {
+            echo '<small>CUSTOMER SUPPORT</small>';
+        }
+        
     echo '</div>';
+    mysqli_free_result($result);
 }
 mysqli_close($conn);
 ?>
--- capstone/admin/php/update_messages.php
+++ capstone/admin/php/update_messages.php
@@ -4,13 +4,29 @@ include("connect.php");
 $data = $_POST['data'];
 $data = explode(' ', $data);
 $sql = '';
+
 if ($data[1]) {
     $date = $data[1] . ' ' . $data[2];
-    $sql = "UPDATE message SET seen = 'Yes' WHERE email = '$data[0]' and deyt = '$date'";
+    $sql = "SELECT seen FROM message WHERE email = '$data[0]' and deyt = '$date'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if ($row['seen'] === 'No') {
+            $sql = "UPDATE message SET seen = 'Yes' WHERE email = '$data[0]' and deyt = '$date'";
+            $result = mysqli_query($conn, $sql);
+        }
+    }
 } else {
-    $sql = "UPDATE message SET seen = 'Yes' WHERE email = '$data[0]' and deyt IS NULL";
+    $sql = "SELECT seen FROM message WHERE email = '$data[0]' and deyt IS NULL";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if ($row['seen'] === 'No') {
+            $sql = "UPDATE message SET seen = 'Yes' WHERE email = '$data[0]' and deyt = '$date'";
+            $result = mysqli_query($conn, $sql);
+        }
+    }
 }
-$result = mysqli_query($conn, $sql);
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
