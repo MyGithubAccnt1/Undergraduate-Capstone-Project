ea4d66011fa40a935ecbd710bf683db071cb7e70
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-02-02 09:28:04 +0800

    Add files via upload

--- capstone/admin/php/get_monthly_earnings.php
+++ capstone/admin/php/get_monthly_earnings.php
@@ -1,8 +1,5 @@
 <?php
 include("connect.php");
-$sqlToday = "SELECT earn FROM earning WHERE DATE(deyt) = CURDATE();";
-$resultToday = mysqli_query($conn, $sqlToday);
-$todayEarn = mysqli_num_rows($resultToday) > 0 ? mysqli_fetch_assoc($resultToday)['earn'] : 0;
 $sql = "SELECT DAY(deyt) AS day, earn
     FROM earning
     WHERE DATE_FORMAT(deyt, '%Y-%m') = DATE_FORMAT(CURDATE(), '%Y-%m')
@@ -12,10 +9,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     $dailyEarnings = array();
     while ($row = mysqli_fetch_assoc($result)) {
-        $dailyEarnings[] = $row['earn'];
-    }
-    if (!$todayEarn) {
-        $dailyEarnings[] = '0';
+        $dailyEarnings[] = floatval($row['earn']);
     }
     echo json_encode(['data' => $dailyEarnings], JSON_NUMERIC_CHECK);
     mysqli_free_result($result);
