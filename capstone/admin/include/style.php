cc13f04441e6b7f882a53b15f38074adf1c873dd
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-02-02 22:16:49 +0800

    Update style.php

--- capstone/admin/include/style.php
+++ capstone/admin/include/style.php
@@ -18,6 +18,8 @@
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
 <!-- chart library -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<!-- generate PDF -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.js"></script>
 <!-- admin side css -->
 <link rel="stylesheet" type="text/css" href="./css/admin_side.css">
 <link rel="stylesheet" type="text/css" href="./css/admin_side_header.css">
