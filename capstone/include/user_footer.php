92528484717204ef49eae0fd7fffc6b01f93b105
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-02-03 16:03:57 +0800

    Add files via upload

--- capstone/include/user_footer.php
+++ capstone/include/user_footer.php
@@ -3,7 +3,7 @@
         <div class="mb-3">
             <img src="images/saint.png" height="40px" alt="Missing_Image">
         </div>
-        <small class="text-center">Copyright © 2023 Saint Benedict Medallion - All Rights Reserved</small>
+        <small class="text-center">Copyright © 2024 Saint Benedict Medallion - All Rights Reserved</small>
         <small class="text-center">A Capstone Project of CvSU Tanza BSIT Students</small>
     </div>
 </footer>
