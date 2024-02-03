2b832b8eb9cc0be4414b6f16e0908440d9ed28c6
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2024-02-03 14:34:28 +0800

    Add files via upload

--- capstone/include/user_footer.php
+++ capstone/include/user_footer.php
@@ -1,10 +1,4 @@
 <footer class="footer">
-    <div class="waves">
-      	<div class="wave" id="wave1"></div>
-      	<div class="wave" id="wave2"></div>
-      	<div class="wave" id="wave3"></div>
-      	<div class="wave" id="wave4"></div>
-    </div>
     <div class="content">
         <div class="mb-3">
             <img src="images/saint.png" height="40px" alt="Missing_Image">
